c9f4af319a3146b7b23ddfa898bfac4c7dbecaab
Hassan Khan <hassan.khan@endemolshine.com>
2018-10-15 00:46:18 +0100

    fix(validation): use subject length of 72 correctly

--- src/validation/validateSubject.ts
+++ src/validation/validateSubject.ts
@@ -2,7 +2,7 @@ export default (input?: string) => {
   if (!input) {
     return 'Must specify subject';
   }
-  if (input.length > 70) {
+  if (input.length > 72) {
     return 'Subject should be 72 characters or less';
   }
   return true;
